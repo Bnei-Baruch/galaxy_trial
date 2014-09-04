eb133d0df0fe56d5b82ca749f8e99996f0825aa6
Konstantin Alexandrov <koalexan@microsoft.com>
2014-09-04 17:34:07 +0200

    Revert "Service unavailable page implemented initially"
    This reverts commit 09c5a65da5afde438b43c29eb0affaf078b0dabc.

--- conference/views.py
+++ conference/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from django.contrib.auth.views import redirect_to_login
 from django.contrib.auth.decorators import login_required
 from django.utils.cache import add_never_cache_headers
-from django.template import RequestContext
 
 from conference import nuve
 
@@ -26,17 +25,11 @@ def _get_role_page(request, role_name, user_name=None):
     """
 
     if request.user.groups.filter(name=role_name).exists():
-        try:
-            token = nuve_client.createToken(
-                room['_id'], user_name or role_name, role_name)
-        except Exception as e:
-            context = dict(error=str(e))
-            context_instance = RequestContext(request)
-            response = render(request, 'service_down.html', context, context_instance=context_instance)
-        else:
-            context = dict(nuve_token=token)
-            response = render(request, '{}.html'.format(role_name), context)
-            add_never_cache_headers(response)
+        token = nuve_client.createToken(
+            room['_id'], user_name or role_name, role_name)
+        context = dict(nuve_token=token)
+        response = render(request, '{}.html'.format(role_name), context)
+        add_never_cache_headers(response)
     else:
         response = redirect_to_login(request.get_full_path())
     return response
