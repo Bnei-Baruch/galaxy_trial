9c04b1f6ca51b25dc5b5f624fec19cfaa8b0f88a
Guy Atia <guy.atia@gmail.com>
2014-09-10 02:53:24 +0000

    Fix black screen when transfer to monitor

--- conference/static/js/monitor.js
+++ conference/static/js/monitor.js
@@ -1,7 +1,14 @@
-function showPreview(html, resizeFunc) {
-    $('#monitor').html(html);
+var curMonitor = '#monitor1';
+var backMonitor = '#monitor2';
 
-    $('.vid').each(function(index) {
+$(document).ready(function(){
+    $(backMonitor).css('opacity', '0');
+});
+
+function loadPreview(html, resizeFunc) {
+    $(backMonitor).html(html);
+     
+    $(backMonitor + ' .vid').each(function(index) {
         var videoId = $(this)[0].id;
         var labelElement = $('#' + videoId + ' label');
         var videoElement = $(this);
@@ -12,3 +19,15 @@ function showPreview(html, resizeFunc) {
     });
 }
 
+function showPreview()
+{
+    $(backMonitor).css('opacity', '1');
+    $(curMonitor).css('opacity', '0');
+    curMonitor = getNextMonitor(curMonitor);
+    backMonitor = getNextMonitor(backMonitor);
+}
+
+
+function getNextMonitor(monitorId) {
+    return (monitorId == '#monitor1' ? '#monitor2' : '#monitor1');
+}
