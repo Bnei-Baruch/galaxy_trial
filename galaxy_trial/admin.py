dee2a1856113b58b9fe75feda213d387f49e6118
Konstantin Alexandrov <negus@negus-laptop>
2014-11-27 18:23:31 +0100

    Custom user model added

--- galaxy_trial/admin.py
+++ galaxy_trial/admin.py
@@ -5,6 +5,8 @@ from galaxy_trial.models import GalaxyUser
 
 
 class GalaxyUserAdmin(UserAdmin):
+    model = GalaxyUser
+
     list_display = UserAdmin.list_display + ('language',)
     fieldsets = UserAdmin.fieldsets + (
         ('Translation', {'fields': ('language',)}),
