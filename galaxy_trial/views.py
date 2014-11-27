e226b0dd3b0041fe11de49243dc86c7e320e8802
Konstantin Alexandrov <negus@negus-laptop>
2014-11-27 18:34:58 +0100

    translator redirect added

--- galaxy_trial/views.py
+++ galaxy_trial/views.py
@@ -7,7 +7,7 @@ def index(request):
     """Redirects users to a view depending on their role.
     """
 
-    for group_name in ('initiator', 'broadcaster'):
+    for group_name in ('initiator', 'broadcaster', 'translator'):
         if request.user.groups.filter(name=group_name).exists():
             return redirect(group_name)
     return redirect('participant')
